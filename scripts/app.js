4642505c3afe4fac9fa69e97237c5bb59d070192
Thr3e <thr3e@Thr3e-MBP.local>
2018-11-13 21:49:10 +0800

    更新主页为scroll视图

--- scripts/app.js
+++ scripts/app.js
@@ -8,43 +8,42 @@ new DateHandler
 new SQLHandler
 new Tools
 $ui.render({
-  props: {
-    title: "Check Box dev",
-    id: "app_main_view",
+  props:{
+    id:"main_view",
     navBarHidden:true,
     statusBarStyle:0,
   },
   views: [{
-    type:"view",
-    props:{
-      clipsToBounds:true,
-      id:"checkbox"
-      
-    },
-    layout:function(make){
-      make.top.equalTo(0)
-      make.left.right.inset(WID * 0.02)
-      make.size.equalTo($size((WID), (100)))
-    },
-    views:[nor_view],
-    events:{
-      tapped(sender){
-        console.log($cache.get("dayList"))
-      }
-    }
-  },{
-    type:"view",
-    props:{
-      clipsToBounds:true,
-      id:"calender",
-      smoothRadius:10
-    },
-    layout:function(make){
-      var wid = WID * 0.8
-      make.top.equalTo($('checkbox').bottom).offset(30)
-      make.left.right.inset((WID - wid) / 2)
-      make.height.equalTo((calender.props.info.cellH * calender.props.info.lines + calender.props.info.titleH * 2 + calender.props.info.infoH))
-    },
-    views:[calender]
+    type: "scroll",
+    layout: $layout.fill,
+    views: [{
+      type:"view",
+      props:{
+        clipsToBounds:true,
+        id:"checkbox"
+      },
+      layout:function(make, view){
+        make.width.equalTo(view.super)
+        make.top.offset(50)
+        make.left.inset(0)
+        make.height.equalTo(100)
+      },
+      views:[nor_view]
+    },{
+      type:"view",
+      props:{
+        clipsToBounds:true,
+        id:"calender",
+        smoothRadius:10
+      },
+      layout:function(make){
+        var wid = WID * 0.8
+        make.top.equalTo($('checkbox').bottom).offset(30)
+        make.left.inset((WID - wid) / 2)
+        make.width.equalTo(wid)
+        make.height.equalTo((calender.props.info.cellH * calender.props.info.lines + calender.props.info.titleH * 2 + calender.props.info.infoH))
+      },
+      views:[calender]
+    }]
   }]
-});
+})
